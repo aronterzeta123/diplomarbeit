7d066688602a23beeed7bbc77a0630754801a549
aronterzeta123 <aron04_t@hotmail.com>
2019-11-08 09:02:20 +0000

    Existiert_nEXistiert script verbessert

--- python_scripts/Existiert_nExistiert.py
+++ python_scripts/Existiert_nExistiert.py
@@ -4,15 +4,18 @@ import MySQLdb
 conn=MySQLdb.connect('localhost','aronterzeta','aronterzeta','test')
 mycursor=conn.cursor()
 var1=input()
+bool=False
 try:
-    mycursor.execute("select * from test1")
+    mycursor.execute("select * from test1 where name='%s'"%(var1))
     myresult=mycursor.fetchall()
     for x in myresult:
         if x[1]==var1:
-            print ("Existiert")
-            break
+            bool=True
         else:
-            print("Nicht existiert")
-            break
+            bool=False
 except:
     print ("Select Statement nicht gut")
+if bool:
+    print("Existiert")
+else:
+    print("Nicht Existiert")
