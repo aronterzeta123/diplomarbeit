7a82196c7bf9f28a36fe594ff1a597736b2ceb96
eglhas14 <eglhas14@htl-shkoder.com>
2019-11-15 03:53:55 -0500

    face detection: modifications

--- python_scripts/faceDetection.py
+++ python_scripts/faceDetection.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
 import cv2
-import numpy as np
+import argparse as prs
+
+ 
+filename = input("insert image file\n")
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-image = cv2.imread('face3.jpeg')
+image = cv2.imread(filename)
 
 gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 
-faces = face_cascade.detectMultiScale(gray,1.1,4)
+face = face_cascade.detectMultiScale(gray,1.1,4)
 
-for(x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (255,0,0), 2) 
+faces = len(face)
 
-cv2.imshow('image',image)
+if faces > 0:
+    for(x, y, w, h) in face:
+        cv2.rectangle(image, (x, y), (x+w, y+h), (255,0,0), 2)
+        cv2.imshow('image',image)
+elif faces <=  0: 
+    print("No faces found")
+
 if cv2.waitKey(0):
     cv2.destroyAllWindows()
