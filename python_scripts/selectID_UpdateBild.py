2d0e3cce2c19c4e9d38a71618716452d969a9e51
Rei Hoxha <reihox14@htl-shkoder.com>
2019-11-22 09:26:29 +0000

    InsertPath in der Tabelle 'info' funktioniert

--- python_scripts/selectID_UpdateBild.py
+++ python_scripts/selectID_UpdateBild.py
@@ -2,20 +2,20 @@
 
 import MySQLdb
 import sys
-conn=MySQLdb.connect('localhost','aronterzeta','aronterzeta','test')
-mycursor=conn.cursor()
-var1=2
 variable3=sys.argv[1]
-try:
+def insertPath(mycursor):
     mycursor.execute("select idP from person where email='%s';"%(variable3))
     myresult=mycursor.fetchall()
     for x in myresult:
         var1=x[0]
+    mycursor.execute("insert into info(imagePath,idP) values('%s',%s);"%('./'+variable3+'.jpg',var1))
+conn=MySQLdb.connect('localhost','aronterzeta','aronterzeta','test')
+mycursor=conn.cursor()
+try:
+    insertPath(mycursor)
+    conn.commit()
     print("Successful")
 except:
+    conn.rollback()
     print("Email nicht gefunden")
-try:
-    mycursor.execute("insert into info(imagePath,idP) values('%s',%s);"%(variable3+'.jpg',var1))
-    print("Insert successful")
-except:
-    print("insert falsch")
+conn.close()
