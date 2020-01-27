5ead40326fb4acaf58524ea4b75d6a668c24d56c
Rei Hoxha <reihox14@htl-shkoder.com>
2020-01-27 00:53:08 +0000

    krahasimi geo.mittel fertig

--- python_scripts/Krahasim2fotot.py
+++ python_scripts/Krahasim2fotot.py
@@ -1,16 +1,20 @@
-#!/usr/bin/python3.5
+#!/usr/bin/python3
 import MySQLdb
 import sys
 import math
+import numpy as np
 conn=MySQLdb.connect('localhost','aronterzeta','aronterzeta','test')
 mycursor=conn.cursor()
 var1=input("Bitte email eingeben")
 b=""
 d=sys.argv[1]
+#varkot=1
+#shumzimi=1
 print(b)
 bool=False
 bool=False
 myresult=""
+geometrischesarray=np.zeros((68,1),dtype="float")
 try:
     mycursor.execute("select * from person p join info i on p.idP = i.idP where p.email='%s'"%(var1))
     global myresult
@@ -30,8 +34,32 @@ if bool:
         vy=5
     exec(open('68fLandmarks.py').read())
     exec(open('68fLandmarks1.py').read())
+    geometrischesarray=1
+    print("Distanca e vjeter:")
     for i in range(0,67):
-        print ( math.sqrt(  ( (vleratx[i]-vleratx1[i]) * (vleratx[i]-vleratx1[i]) ) + ( (vleraty[i]-vleraty1[i]) * (vleraty[i]-vleraty1[i]) ) ) )
+        a=math.sqrt(  ( (vleratx[i]-vleratx1[i]) * (vleratx[i]-vleratx1[i]) ) + ( (vleraty[i]-vleraty1[i]) * (vleraty[i]-vleraty1[i]) ) )
+        #varkot=i-1
+        #print((geometrischesarray[varkot]*geometrischesarray[i])**(1/67))
+        #shumzimi=geometrischesarray[i]*geometrischesarray[varkot]
+        #varkot+=1
+    #print(shumzimi)a
+        #print(math.sqrt(  ( (vleratx[i]-vleratx1[i]) * (vleratx[i]-vleratx1[i]) ) + ( (vleraty[i]-vleraty1[i]) * (vleraty[i]-vleraty1[i]) ) ))
+        print(a)
+        geometrischesarray *= a
+        #print(geometrischesarray)
+    print("Shumzim")
+    print(geometrischesarray)
+    print("Toleranca me geo.Mittel")
+    t1=geometrischesarray**(1/67)
+    print(t1)
+    #print("Toleranca me MAX")
+    #print(max
+    #print("67")
+    #print(geometrischesarray[67])
+    #t2=max(a)
+    #print(t2)
+    #tFinal=(t1+t2)/2
+    #print(tFinal)
 else:
     #os.system('./Log_Erkennung_PersonExistiertNicht.py')
     b="nicht existiert"
