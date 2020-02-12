4ccec1acd0b67977cdaa675d254f81c5ce47433e
root <root@localhost.localdomain>
2020-02-12 23:22:58 +0100

    blink1.py

---
+++ python_scripts/3D/blink1.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+from __future__ import division
+import dlib
+from imutils import face_utils
+import cv2
+import numpy as np
+from scipy.spatial import distance as dist
+
+def resize(img, width=None, height=None, interpolation=cv2.INTER_AREA):
+    global ratio
+    w, h = img.shape
+    if width is None and height is None:
+        return img
+    elif width is None:
+        ratio = height / h
+        width = int(w * ratio)
+        resized = cv2.resize(img, (height, width), interpolation)
+        return resized
+    else:
+        ratio = width / w
+        height = int(h * ratio)
+        resized = cv2.resize(img, (height, width), interpolation)
+        return resized
+
+def shape_to_np(shape, dtype="int"):
+    coords = np.zeros((68, 2), dtype=dtype)
+    for i in range(36,48):
+        coords[i] = (shape.part(i).x, shape.part(i).y)
+    return coords
+
+def eye_aspect_ratio(eye):
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+    C = dist.euclidean(eye[0], eye[3])
+    ear = (A + B) / (2.0 * C)
+    return ear
+camera = cv2.VideoCapture(0)
+
+predictor_path = 'shape_predictor_68_face_landmarks.dat'
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(predictor_path)
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+detected=0
+
+while True:
+    ret, frame = camera.read()
+   
+    if detected>0:
+        print ("Person detected")
+        break
+    if ret == False:
+        print('Failed to capture.')
+        break;
+
+    frame_grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame_resized = resize(frame_grey, width=120)
+
+    dets = detector(frame_resized, 1)
+    
+    if len(dets) > 0:
+        for k, d in enumerate(dets):
+            shape = predictor(frame_resized, d)
+            shape = shape_to_np(shape)
+            leftEye= shape[lStart:lEnd]
+            rightEye= shape[rStart:rEnd]
+            leftEAR= eye_aspect_ratio(leftEye)
+            rightEAR = eye_aspect_ratio(rightEye)
+            ear = (leftEAR + rightEAR) / 2.0
+            
+            if ear>.29:
+                detected=1
+camera.release()
+#Thirre scriptin itu
+#exec(open("asdfasd.py").read())
